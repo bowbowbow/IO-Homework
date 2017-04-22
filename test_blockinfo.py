3284cf7109c13d037969657634a6ffac6148a66f
Seunghun Lee <waydi1@gmail.com>
2017-04-23 00:17:25 +0900

    Add test for number of transactions to make fail

--- test_blockinfo.py
+++ test_blockinfo.py
@@ -10,3 +10,7 @@ def test_n_tx():
     hash_value = '000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f'
     block_info = BlockInfo(hash_value)
     assert block_info.n_tx == 1
+
+    hash_value = '0000000000000e102232eeb35175920438b8d8de12cab20f3e4d5b6e56dbca95'
+    block_info = BlockInfo(hash_value)
+    assert block_info.n_tx == 9
