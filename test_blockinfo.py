1c9c7d6735e93a1a329649544b0230d684dad3b7
Seunghun Lee <waydi1@gmail.com>
2017-04-22 23:32:35 +0900

    Add test for get_api_url

--- test_blockinfo.py
+++ test_blockinfo.py
@@ -1,2 +1,6 @@
-def test_dummy():
-    assert True == True
+from blockinfo import get_api_url
+
+def test_get_api_url():
+    hash_value = '000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f'
+
+    assert get_api_url(hash_value) == 'https://blockchain.info/block/000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f?format=json'
