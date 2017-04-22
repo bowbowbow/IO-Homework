86975fe8669736d42362867ee626234d58e1a38b
Seunghun Lee <waydi1@gmail.com>
2017-04-23 01:28:59 +0900

    Get average value of transactions from 'tx' in block

--- blockinfo.py
+++ blockinfo.py
@@ -8,6 +8,7 @@ class BlockInfo():
         self.api_url = self.get_api_url(hash_value)
         self.block = self.get_block(self.api_url)
         self.n_tx = self.get_n_tx(self.block)
+        self.avg_tx_value = self.get_avg_tx_value(self.n_tx, self.block['tx'])
 
     def get_api_url(self, hash_value):
         api_url = 'https://blockchain.info/block/{}?format=json'
@@ -19,3 +20,11 @@ class BlockInfo():
 
     def get_n_tx(self, block):
         return block['n_tx']
+
+    def get_avg_tx_value(self, n_tx, transactions):
+        value = 0
+        for tx in transactions:
+            for out in tx['out']:
+                value += out['value']
+
+        return round(value / n_tx * 1e-8, 8)
