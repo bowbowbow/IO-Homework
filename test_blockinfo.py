bb4b0fe9d1eb3df86aad870184c3f601e753c946
Seunghun Lee <waydi1@gmail.com>
2017-04-23 01:16:04 +0900

    Add test for average value of transactions
    It should be compared up to 8 decimal digits. Because it is the smallest
    unit(Satoshi) in Bitcoin.

--- test_blockinfo.py
+++ test_blockinfo.py
@@ -1,3 +1,4 @@
+import math
 from blockinfo import BlockInfo
 
 def test_get_api_url():
@@ -14,3 +15,12 @@ def test_n_tx():
     hash_value = '0000000000000e102232eeb35175920438b8d8de12cab20f3e4d5b6e56dbca95'
     block_info = BlockInfo(hash_value)
     assert block_info.n_tx == 9
+
+def test_avg_tx_value():
+    hash_value = '000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f'
+    block_info = BlockInfo(hash_value)
+    assert math.isclose(block_info.avg_tx_value, 50, rel_tol=1e-8)
+
+    hash_value = '0000000000000e102232eeb35175920438b8d8de12cab20f3e4d5b6e56dbca95'
+    block_info = BlockInfo(hash_value)
+    assert math.isclose(block_info.avg_tx_value, 39.65358258, rel_tol=1e-8)
