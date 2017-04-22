df40f930be46b67801e0587a7cea6cf48ba15b82
Seunghun Lee <waydi1@gmail.com>
2017-04-23 00:34:48 +0900

    Get block from API and store in the object

--- blockinfo.py
+++ blockinfo.py
@@ -1,11 +1,18 @@
 """Parse Bitcoin Block Information from blockchain.info API
 """
+import json
+from urllib.request import urlopen
 
 class BlockInfo():
     def __init__(self, hash_value):
         self.api_url = self.get_api_url(hash_value)
+        self.block = self.get_block(self.api_url)
         self.n_tx = 1
 
     def get_api_url(self, hash_value):
         api_url = 'https://blockchain.info/block/{}?format=json'
         return api_url.format(hash_value)
+
+    def get_block(self, api_url):
+        data = urlopen(api_url).read().decode()
+        return json.loads(data)
