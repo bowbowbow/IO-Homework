4aab8f7b89466ba40cab01b544838c40c57c3094
Seunghun Lee <waydi1@gmail.com>
2017-04-22 23:56:44 +0900

    Use class for modeling block information

--- blockinfo.py
+++ blockinfo.py
@@ -1,3 +1,7 @@
-def get_api_url(hash_value):
-    api_url = 'https://blockchain.info/block/{}?format=json'
-    return api_url.format(hash_value)
+class BlockInfo():
+    def __init__(self, hash_value):
+        self.api_url = self.get_api_url(hash_value)
+
+    def get_api_url(self, hash_value):
+        api_url = 'https://blockchain.info/block/{}?format=json'
+        return api_url.format(hash_value)
--- test_blockinfo.py
+++ test_blockinfo.py
@@ -1,6 +1,7 @@
-from blockinfo import get_api_url
+from blockinfo import BlockInfo
 
 def test_get_api_url():
     hash_value = '000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f'
+    block_info = BlockInfo(hash_value)
 
-    assert get_api_url(hash_value) == 'https://blockchain.info/block/000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f?format=json'
+    assert block_info.api_url == 'https://blockchain.info/block/000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f?format=json'
